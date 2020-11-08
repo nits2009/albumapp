19e5f43fc268c246671135be2d3b6aa4758f45e7
nitin <nitin.jhare@fareportal.com>
2020-11-08 21:34:57 +0530

    Initial code setup

---
+++ ios/Pods/Target Support Files/Pods-MyAlbumApp-MyAlbumAppTests/Pods-MyAlbumApp-MyAlbumAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyAlbumApp_MyAlbumAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_MyAlbumApp_MyAlbumAppTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-MyAlbumApp-tvOSTests/Pods-MyAlbumApp-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyAlbumApp_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_MyAlbumApp_tvOSTests
+@end
